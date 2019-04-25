dc2d4bb16f5ca4ea110ccf2cd2956813a6ff9a08
dbelmo2 <49499409+dbelmo2@users.noreply.github.com>
2019-04-25 11:51:03 -0500

    gui update

--- triviaClient/src/sample/Main.java
+++ triviaClient/src/sample/Main.java
@@ -1,7 +1,6 @@
 package sample;
 
 import java.util.ArrayList;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -14,6 +13,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
 public class Main extends Application {
 
     BorderPane mainLayout, gameLayout;
@@ -26,7 +26,14 @@ public class Main extends Application {
     private ClientNetwork conn = createClient("127.0.0.1", 5555);
     private TextArea messages = new TextArea();
     public Parent setupGUI() {
-    	messages.setPrefHeight(300);
+
+        messages.setPrefHeight(300);
+
+        messages.getStylesheets().add("textArea.css");
+        messages.setMaxSize(650, 200);
+        messages.setOpaqueInsets(new Insets(0));
+        messages.setBorder(null);
+
         //lobby background setup
         lobbyBackground = new Image("triviaLobby.png");
         BackgroundImage backGround1 = new BackgroundImage(lobbyBackground,
@@ -70,37 +77,37 @@ public class Main extends Application {
         answerButton[2].setText("C");
         answerButton[3].setText("D");
         EventHandler<ActionEvent> choice = event -> {
-			
-			// disable buttons when choice is made
-			answerButton[0].setDisable(true);
-			answerButton[1].setDisable(true);
-			answerButton[2].setDisable(true);
-			answerButton[3].setDisable(true);
-			
-			String client = "Your choice was: ";
-			messages.clear();
-			String message = "";
-			
-			if(event.getSource() == answerButton[0])
-				message += "A";
-			else if(event.getSource() == answerButton[1])
-				message += "B";
-			else if(event.getSource() == answerButton[2])
-				message += "C";
-			else if(event.getSource() == answerButton[3])
-				message += "D";
-			
-			messages.appendText(client + message + "\n");
-			try {
-				conn.send(message);
-			}
-			catch(Exception e) {
-				
-			}
-		};
+
+            // disable buttons when choice is made
+            answerButton[0].setDisable(true);
+            answerButton[1].setDisable(true);
+            answerButton[2].setDisable(true);
+            answerButton[3].setDisable(true);
+
+            String client = "Your choice was: ";
+            messages.clear();
+            String message = "";
+
+            if(event.getSource() == answerButton[0])
+                message += "A";
+            else if(event.getSource() == answerButton[1])
+                message += "B";
+            else if(event.getSource() == answerButton[2])
+                message += "C";
+            else if(event.getSource() == answerButton[3])
+                message += "D";
+
+            messages.appendText(client + message + "\n");
+            try {
+                conn.send(message);
+            }
+            catch(Exception e) {
+
+            }
+        };
         for(int i=0; i< 4; i++) {
-        	answerButton[i].setDisable(true);
-        	answerButton[i].setOnAction(choice);
+            answerButton[i].setDisable(true);
+            answerButton[i].setOnAction(choice);
         }
         // adding elements to lobby layout
         mainLayout = new BorderPane();
@@ -112,12 +119,8 @@ public class Main extends Application {
         //adding elements to the game layout
         gameLayout = new BorderPane();
         gameLayout.setPadding(new Insets(25));
-        HBox choices = new HBox(20, answerButton[0], answerButton[1]);
-		HBox choices2 = new HBox(20, answerButton[2], answerButton[3]);
-		VBox buttons = new VBox(20,choices, choices2); 
-		VBox root = new VBox(20, messages);
-		gameLayout.setCenter(root);
-        gameLayout.setBottom(buttons);
+        gameLayout.setCenter(messages);
+        gameLayout.setBottom(gameButtons);
         gameLayout.setBackground(new Background(backGround2));
 
         //scene2 setup
@@ -132,79 +135,79 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-    	update = new Button();
+        update = new Button();
         stage = primaryStage;
         stage.setTitle("Trivia");
-        
+
         update.setOnAction(e -> {
-			messages.clear();
-			//might need this to update but its useless code tbh
-			for(int i = 0; i < answerButton.length; i++) {
-				answerButton[i].setText(answerButton[i].getText());
-			}
-			//primaryStage.setScene(new Scene(createClientContent()));
-		});
-        
+            messages.clear();
+            //might need this to update but its useless code tbh
+            for(int i = 0; i < answerButton.length; i++) {
+                answerButton[i].setText(answerButton[i].getText());
+            }
+            //primaryStage.setScene(new Scene(createClientContent()));
+        });
+
         scene1 = new Scene(setupGUI(), 795, 445);
         stage.setScene(scene1);
-        
-        
-        
+
+
+
         stage.show();
     }
 
     @Override
-	public void init() throws Exception{
-		conn.startConn();
-	}
-	
-	@Override
-	public void stop() throws Exception{
-		conn.closeConn();
-	}
+    public void init() throws Exception{
+        conn.startConn();
+    }
+
+    @Override
+    public void stop() throws Exception{
+        conn.closeConn();
+    }
     public static void main(String[] args) {
         launch(args);
     }
     private Client createClient(String address, int port) {
-		return new Client(address, port, data -> {
-			Platform.runLater(()->{
-				messages.appendText(data.toString() + "\n");
-				
-				if(data.toString().intern().contains("A: ")) {
-					System.out.println("yup");
-					answerButton[0].setText(data.toString().intern());
-					update.fire();
-				}
-				if(data.toString().intern().contains("B: ")) {
-					answerButton[1].setText(data.toString().intern());
-					update.fire();
-				}
-				if(data.toString().intern().contains("C: ")) {
-					answerButton[2].setText(data.toString().intern());
-					update.fire();
-				}
-				if(data.toString().intern().contains("D: ")) {
-					answerButton[3].setText(data.toString().intern());
-					update.fire();
-				}
-				
-				// if opponent joined the game room
-				if(data.toString().intern() == "Game has started") {
-					// enable game buttons
-					for(int i = 0; i<answerButton.length;i++) {
-						answerButton[i].setDisable(false);
-					}
-					
-					
-				}
-				
+        return new Client(address, port, data -> {
+            Platform.runLater(()->{
+                messages.appendText(data.toString() + "\n");
 
-				// parses the server message into an array of words
-				String serverMessage[] =  data.toString().split(" ");
-				ArrayList<String> updatedConnections = new ArrayList<String>();
-				for(String word : data.toString().split(" ")) {
-					updatedConnections.add(word);
-				}
+                if(data.toString().intern().contains("A: ")) {
+                    System.out.println("yup");
+                    answerButton[0].setText(data.toString().intern());
+                    update.fire();
+                }
+                if(data.toString().intern().contains("B: ")) {
+                    answerButton[1].setText(data.toString().intern());
+                    update.fire();
+                }
+                if(data.toString().intern().contains("C: ")) {
+                    answerButton[2].setText(data.toString().intern());
+                    update.fire();
+                }
+                if(data.toString().intern().contains("D: ")) {
+                    answerButton[3].setText(data.toString().intern());
+                    update.fire();
+                }
+
+                // if opponent joined the game room
+                if(data.toString().intern() == "Game has started") {
+                    // enable game buttons
+                    for(int i = 0; i<answerButton.length;i++) {
+                        answerButton[i].setDisable(false);
+                    }
+
+
+                }
+
+
+                // parses the server message into an array of words
+                String serverMessage[] =  data.toString().split(" ");
+                ArrayList<String> updatedConnections = new ArrayList<String>();
+                for(String word : data.toString().split(" ")) {
+                    updatedConnections.add(word);
+                }
 				/*
 				// if first word of messaage is ID, set this.ID = to the corresponding ID (serverMessage[1])
 				if(serverMessage[0].intern() == "ID") {
@@ -212,8 +215,7 @@ public class Main extends Application {
 					idLabel.setText("ID: " + serverMessage[1]);
 				}*/
 
-	
-			});
-		});
-	}
-}
+
+            });
+        });
+    }
